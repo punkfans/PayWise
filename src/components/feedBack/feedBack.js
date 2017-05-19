80186598ed545dcbaf88916c5ebb9010ae012b9f
punkfans <stevenyuyang@gmail.com>
2017-05-19 16:51:07 -0500

    feed back page setup, style and functionality undone

--- src/components/feedBack/feedBack.js
+++ src/components/feedBack/feedBack.js
@@ -4,5 +4,34 @@
     angular.module('app')
         .controller('feedBackController', feedBackController);
 
-    function feedBackController() {}
+    function feedBackController($scope) {
+
+        $scope.images = [
+            {
+                url: 'src/img/1.svg',
+                value: 1,
+                halfOpacity: false
+            },
+            {
+                url: 'src/img/2.svg',
+                value: 2,
+                halfOpacity: false
+            },
+            {
+                url: 'src/img/3.svg',
+                value: 3,
+                halfOpacity: false
+            },
+            {
+                url: 'src/img/4.svg',
+                value: 4,
+                halfOpacity: false
+            },
+            {
+                url: 'src/img/5.svg',
+                value: 5,
+                halfOpacity: false
+            }
+        ];
+    }
 })();
